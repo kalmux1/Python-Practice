2b51c0984d163b54a0523a59e21195d37e7c8312
kalmux1 <kal.mux.cyber@gmail.com>
2024-03-03 18:39:56 +0530

    TMS

--- tms.py
+++ tms.py
@@ -5,3 +5,20 @@
 print(" TAX MANAGEMENT SYSTEM ")
 print(" ")
 salary=float(input(" ENTER YOUR SALARY :- "))
+
+match salary:
+
+    case _ if salary >= 20000:
+        print(" YOU DON'T HAVE TO PAY TAX")
+        
+    case _  if salary >= 20000 and salary < 50000:
+        print(" YOU HAVE TO PAY 10% TAX")
+
+    case _ if salary >= 50000 and salary < 75000:
+        print(" YOU HAVE TO PAY 15% TAX")
+
+    case _ if salary >= 75000 and salary < 90000:
+        print(" YOU HAVE TO PAY 20% TAX")
+
+    case _ if salary <= 90000:
+        print(" YOR HAVE TO PAY 25% TAX ")
