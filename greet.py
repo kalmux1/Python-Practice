c98751054f630c58aee1fe05afcbe48794976faf
kalmux1 <kal.mux.cyber@gmail.com>
2024-03-03 18:20:35 +0530

    GREET

---
+++ greet.py
@@ -0,0 +1,19 @@
+
+# * GREETING PROGRAM IN PYTHON
+
+import time
+
+timestamp=int(time.strftime('%H'))
+
+print(" KALMUX AI ")
+
+if(timestamp >= 22 and timestamp < 5):
+    print(" GOOD NIGHT SIR !")
+elif(timestamp >= 12 and timestamp < 16):
+    print(" GOOD AFTERNOON SIR !")
+elif(timestamp >= 16 and timestamp < 22):
+    print(" GOOD EVENING SIR !")
+elif(timestamp >= 5 and timestamp < 12):
+    print(" GOOD MORING SIR !")
+else:
+    print(" AI ERROR SIR !")
