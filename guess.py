9efe2977497cb4ba190986b131009d0171d005b4
kalmux1 <kal.mux.cyber@gmail.com>
2024-06-28 18:21:18 +0530

    Guess Game

--- guess.py
+++ guess.py
@@ -2,24 +2,6 @@ import random
 
 number = random.randint(1,100)
 
-print(number)
-
-
-# def game(guess):
-#     count = 0
-
-#     if (guess == number):
-#         print(f"You Guessed The Right Number {number} in {count} guesses")
-#         return False
-#     elif (guess > number):
-#         print("Lower Number Please")
-#         count += 1
-#     elif (guess < number):
-#         print("Higher Number Please")
-#         count += 1
-#     else:
-#         print("Invalid Input")
-
 gamerun = True
 
 count = 1
