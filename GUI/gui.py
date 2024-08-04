d3de8b528e45e04ee04007dbad1a11d0b9037d4d
kalmux1 <kal.mux.cyber@gmail.com>
2024-08-04 15:22:10 +0530

    GUI Practice

--- GUI/gui.py
+++ GUI/gui.py
@@ -2,16 +2,29 @@
 
 from tkinter import *
 
-root = Tk()
+base = Tk()
 
-root.title("KALMUX")
+base.title("KALMUX")
 
-root.geometry("833x534")
+base.geometry("733x434")
 
-photo = PhotoImage(file="E:\GITHUB\Python-Practice\GUI\wp.png")
-bg = Label(image=photo)
-bg.pack()
-kalmux = Label(root, text="KALMUX GUI", padx=10, pady=10, relief=RAISED)
-kalmux.pack(anchor="center")
+# photo = PhotoImage(file="E:\GITHUB\Python-Practice\GUI\wp.png")
+# bg = Label(image=photo)
+# bg.pack()
 
-root.mainloop()
+frame1 = Frame(base, borderwidth=9 , padx=210 , relief=SUNKEN , bg="lightgrey")
+frame1.pack(side=TOP, fill=X)
+kalmux = Label(frame1, text="KALMUX GUI", fg="black", bg="lightgrey")
+kalmux.pack(anchor="center", padx=110)
+
+frame2 = Frame(base, relief=SUNKEN, borderwidth=3, pady=180, padx=25, bg="lightgrey")
+frame2.pack(side=LEFT, fill=Y)
+content = Label(frame2, text="CONTENT", fg="black", bg="lightgrey")
+content.pack(side=TOP, anchor="ne")
+
+frame3 = Frame(base,borderwidth=3, relief=SUNKEN, bg="lightgrey")
+frame3.pack(anchor="center")
+
+b1 = Button(frame3, text="button")
+b1.pack(anchor="center")
+base.mainloop()
