05ddaa23fcb1f7cacbe5a20059e82642b87a08b3
kalmux1 <kal.mux.cyber@gmail.com>
2024-08-05 00:20:54 +0530

    form

--- GUI/addmission-form.py
+++ GUI/addmission-form.py
@@ -4,7 +4,7 @@ from tkinter import *
 
 base = Tk()
 
-
+base.configure(bg="darkgrey")
 base.title(" COLLEGE ADMISSION FORM ")
 
 base.geometry("600x400")
@@ -14,44 +14,52 @@ head_frame = Frame(base, relief=SUNKEN, bg="lightgrey", borderwidth=4,padx=207)
 head_frame.pack(side=TOP)
 
 head_tittle = Label(head_frame, text=" COLLEGE ADMISSION FORM ", fg="black",bg="lightgrey")
-head_tittle.pack(pady=2,)
+head_tittle.pack(pady=2)
 
-body_frame = Frame(base, relief=SUNKEN, borderwidth=4, padx=190 , pady= 120)
-body_frame.pack(pady=10,anchor="center")
+body_frame = Frame(base, relief=SUNKEN, borderwidth=4, padx=20 , pady= 50)
+body_frame.pack(padx=6,pady=10, fill=BOTH, expand=True)
 
 Fname = Label(body_frame, text=" FIRST NAME ")
-Fname.grid(row=1, sticky="nw")
+Fname.grid(row=1,column=0, pady= 5,padx=10, sticky=E)
 
 fnamevalue = StringVar()
-fnameinput = Entry(body_frame, textvariable= fnamevalue)
-fnameinput.grid(row=1, column=1)
+fnameinput = Entry(body_frame, textvariable= fnamevalue, width=25)
+fnameinput.grid(row=1, column=1, padx= 5)
 
 Lname = Label(body_frame, text=" LAST NAME ")
-Lname.grid()
+Lname.grid(row=1,column=3, pady=5,padx=10, sticky=E)
 
 lnamevalue = StringVar()
-lnameinput = Entry(body_frame, textvariable= lnamevalue)
-lnameinput.grid(row=2, column=1)
+lnameinput = Entry(body_frame, textvariable= lnamevalue,width=25)
+lnameinput.grid(row=1, column=4, padx=5)
 
-Age = Label(body_frame, text=" FIRST NAME ")
-Age.grid()
+Age = Label(body_frame, text=" AGE ")
+Age.grid(row=2 ,padx=10,pady=5)
 
 agevalue = StringVar()
-ageinput = Entry(body_frame, textvariable= agevalue)
-ageinput.grid(row=3, column=1)
+ageinput = Entry(body_frame, textvariable= agevalue,width=25)
+ageinput.grid(row=2, column=1, pady=10)
+
+
+dob = Label(body_frame, text=" DOB ")
+dob.grid(row=2,column=3)
+
+dobvalue = StringVar()
+dobinput = Entry(body_frame, textvariable= dobvalue, width=25)
+dobinput.grid(row=2, column=4, pady=5,padx=10)
 
 add = Label(body_frame, text=" ADDRESS ")
-add.grid()
+add.grid(row=3, padx=10,pady=5)
 
 addvalue = StringVar()
-addinput = Entry(body_frame, textvariable= addvalue)
-addinput.grid(row=4, column=1)
+addinput = Entry(body_frame, textvariable= addvalue, width= 69)
+addinput.grid(row=3, column=1,columnspan=4)
 
 def getvals():
     pass
 
 
-submit = Button(body_frame, text="SUBMIT" , command=getvals)
-submit.grid(row=5,column=1, pady=10)
+submit = Button(body_frame, text="SUBMIT" , command=getvals ,padx=15,relief=RAISED)
+submit.grid(row=4,column=0, pady=80, columnspan=5)
 
 base.mainloop()
